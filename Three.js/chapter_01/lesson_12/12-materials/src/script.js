b0af4e58b170304deb35103145a47819a14fa680
Florianne Coudert <coudert.florianne.69@gmail.com>
2022-03-08 17:14:13 +0100

    Blender: donuts animation + Three.js lesson 1.13

--- Three.js/chapter_01/lesson_12/12-materials/src/script.js
+++ Three.js/chapter_01/lesson_12/12-materials/src/script.js
@@ -1,6 +1,43 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'dat.gui'
+
+/**
+ * 
+ */
+const gui = new dat.GUI()
+
+/**
+ * Textures
+ */
+
+// const loadingManager = THREE.LoadingManager()
+
+const textureLoader = new THREE.TextureLoader()
+const cubeTextureLoader = new THREE.CubeTextureLoader()
+
+const doorColorTexture = textureLoader.load('/textures/door/color.jpg')
+const doorAlphaTexture = textureLoader.load('/textures/door/alpha.jpg')
+const doorAmbientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg')
+const doorHeightTexture = textureLoader.load('/textures/door/height.jpg')
+const doorNormalTexture = textureLoader.load('/textures/door/normal.jpg')
+const doorMetalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
+const doorRoughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
+const matcapTexture = textureLoader.load('/textures/matcaps/8.png')
+const gradientTexture = textureLoader.load('/textures/gradients/3.jpg')
+gradientTexture.minFilter = THREE.NearestFilter // to fix the mipmapping of the gradient
+gradientTexture.magFilter = THREE.NearestFilter
+gradientTexture.generateMipmaps = false
+
+const environmentMapTexture = cubeTextureLoader.load([
+    '/textures/environmentMaps/1/px.jpg',
+    '/textures/environmentMaps/1/nx.jpg',
+    '/textures/environmentMaps/1/py.jpg',
+    '/textures/environmentMaps/1/ny.jpg',
+    '/textures/environmentMaps/1/pz.jpg',
+    '/textures/environmentMaps/1/nz.jpg'
+])
 
 /**
  * Base
@@ -10,6 +47,118 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
+
+/**
+ * Objects
+ */
+
+
+// const material = new THREE.MeshBasicMaterial()
+
+// material.map = doorColorTexture
+// material.color = new THREE.Color('pink')
+// material.color.set('#ff00ff')
+// material.wireframe = true
+// material.opacity = 0.5 //to do with transparency
+// material.transparent = true
+// material.alphaMap = doorAlphaTexture // black = hidden white = shown to do with transparency
+// material.side = THREE.DoubleSide // for plane only front side is shown also THREE.FrontSide and THREE.BackSide
+
+// const material = new THREE.MeshNormalMaterial() // Normal are information that contains the direction of outside of the face
+// material.flatShading = true
+
+// const material = new THREE.MeshMatcapMaterial() // use the normals as a reference to picck the right rexture that looks like a sphere
+// material.matcap = matcapTexture // you can create matcap in blender
+
+// const material = new THREE.MeshDepthMaterial() //color in white if it's closed to the camera
+
+// const material = new THREE.MeshLambertMaterial()
+
+// const material = new THREE.MeshPhongMaterial()
+// material.shininess = 1000
+// material.specular = new THREE.Color(0x1188ff)
+
+// const material = new THREE.MeshToonMaterial()
+// material.gradientMap = gradientTexture
+
+// const material = new THREE.MeshStandardMaterial()
+// material.metalness =0
+// material.roughness = 0
+// material.aoMapIntensity = 1
+// material.displacementMap = doorHeightTexture
+// material.displacementScale = 0.05
+// material.metalnessMap = doorMetalnessTexture
+// material.roughnessMap = doorRoughnessTexture
+// material.normalMap=doorNormalTexture
+// material.normalScale.set(0.5, 0.5)
+// material.transparent = true
+// material.alphaMap = doorAlphaTexture
+
+const material = new THREE.MeshStandardMaterial()
+material.metalness =0.7
+material.roughness = 0.2
+material.envMap = environmentMapTexture
+
+
+gui.add(material, 'metalness').min(0).max(1).step(0.0001)
+gui.add(material, 'roughness').min(0).max(1).step(0.0001)
+gui.add(material, 'aoMapIntensity').min(0).max(10).step(0.0001)
+gui.add(material, 'displacementScale').min(0).max(5).step(0.0001)
+
+// material.map = doorColorTexture
+// material.aoMap = doorAmbientOcclusionTexture
+
+
+
+const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5,64,64),
+    material
+)
+
+sphere.position.x += - 1.5
+
+sphere.geometry.setAttribute(
+    'uv2',
+    new THREE.BufferAttribute(sphere.geometry.attributes.uv.array, 2)
+) 
+
+const plane = new THREE.Mesh(
+    new THREE.PlaneGeometry(1,1, 100 , 100),
+    material
+)
+
+plane.geometry.setAttribute(
+    'uv2',
+    new THREE.BufferAttribute(plane.geometry.attributes.uv.array, 2)
+) // three js need this for ambient occlusion
+
+const torus = new THREE.Mesh(
+    new THREE.TorusGeometry( 0.3, 0.2, 64, 128),
+    material
+)
+
+torus.geometry.setAttribute(
+    'uv2',
+    new THREE.BufferAttribute(torus.geometry.attributes.uv.array, 2)
+) 
+
+torus.position.x = 1.5
+
+scene.add(sphere, plane, torus)
+
+/**
+ * Light
+ */
+
+const ambientLight = new THREE.AmbientLight (0xffffff, 0.5)
+scene.add(ambientLight)
+
+const pointLight = new THREE.PointLight(0xffffff, 0.5)
+pointLight.position.x = 2
+pointLight.position.y = 3
+pointLight.position.z = 4
+scene.add(pointLight)
+
 
 /**
  * Sizes
@@ -65,6 +214,16 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    // Update objects
+    sphere.rotation.y = 0.1 * elapsedTime
+    plane.rotation.y = 0.1 * elapsedTime
+    torus.rotation.y = 0.1 * elapsedTime
+
+    sphere.rotation.x = 0.15 * elapsedTime
+    plane.rotation.x = 0.15 * elapsedTime
+    torus.rotation.x = 0.15 * elapsedTime
+
 
     // Update controls
     controls.update()
