bf2efb4267bd87ff5d1e024e12a298f85f6e0f32
Florianne Coudert <coudert.florianne.69@gmail.com>
2022-03-18 22:20:57 +0100

    Blender: cupcake uv mapping + Three.js lesson 3.1

--- Three.js/chapter_02/lesson_21/21-scroll-based-animation/src/script.js
+++ Three.js/chapter_02/lesson_21/21-scroll-based-animation/src/script.js
@@ -1,6 +1,7 @@
 import './style.css'
 import * as THREE from 'three'
 import * as dat from 'lil-gui'
+import gsap from 'gsap'
 
 /**
  * Debug
@@ -16,6 +17,7 @@ gui
     .onChange(() => 
     {
         material.color.set(parameters.materialColor)
+        particlesMaterial.color.set(parameters.materialColor)
     })
 
 /**
@@ -62,6 +64,36 @@ mesh3.position.x = 2
 scene.add(mesh1, mesh2, mesh3)
 
 const sectionMeshes = [mesh1, mesh2, mesh3]
+
+/**
+ * Particles
+ */
+
+const particlesCount = 500
+const positions = new Float32Array(particlesCount * 3)
+for (let i = 0; i< particlesCount; i++)
+{
+    positions[i * 3 ] = (Math.random() - 0.5) * 11
+    positions[i * 3 + 1] = objectsDistance * 0.5 - Math.random()  * objectsDistance * sectionMeshes.length
+    positions[i * 3 + 2] = (Math.random() - 0.5) * 10
+}
+
+const particlesGerometry = new THREE.BufferGeometry()
+particlesGerometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
+const particlesMaterial = new THREE.PointsMaterial({
+    color:parameters.materialColor,
+    sizeAttenuation: true,
+    size:0.03,
+})
+
+
+const particles = new THREE.Points(particlesGerometry, particlesMaterial)
+
+scene.add(particles)
+
+/**
+ * Lights
+ */
 
 const directionalLight = new THREE.DirectionalLight('#ffffff',1)
 directionalLight.position.set(1,1,0)
@@ -115,11 +147,30 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  * Scroll
  */
 
-let ScrollY = window.scrollY
+let scrollY = window.scrollY
+let currentSection = 0
 
 window.addEventListener('scroll', () =>
 {
-    console.log('user is scrolling')
+    scrollY = window.scrollY
+
+    const newSection = Math.round(scrollY / sizes.height)
+
+    if(newSection != currentSection)
+    {
+        currentSection = newSection
+
+        gsap.to(
+            sectionMeshes[currentSection].rotation,
+            {
+                duration: 1.5,
+                ease: 'power2.inOut',
+                x: '+=6',
+                y: '+=3',
+                z: '+=1.5'
+            }
+        )
+    }
 })
 
 /**
@@ -142,24 +193,29 @@ window.addEventListener('mousemove', (_event) =>
  * Animate
  */
 const clock = new THREE.Clock()
+let previousTime = 0
 
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+    const deltaTime = elapsedTime - previousTime
+    previousTime = elapsedTime
+
+    // console.log(deltaTime)
 
     //animate camera
     camera.position.y = -scrollY /sizes.height *objectsDistance
 
-    const parallaxX = cursor.x ;
-    const parallaxY = - cursor.y ;
-    cameraGroup.position.x = parallaxX
-    cameraGroup.position.y = parallaxY
+    const parallaxX = cursor.x * 0.5;
+    const parallaxY = - cursor.y * 0.5;
+    cameraGroup.position.x += (parallaxX  - cameraGroup.position.x) * 5 * deltaTime
+    cameraGroup.position.y += (parallaxY - cameraGroup.position.y) * 5 * deltaTime
 
     //animate meshes
     for(const mesh of sectionMeshes)
     {
-        mesh.rotation.x = elapsedTime * 0.1
-        mesh.rotation.y = elapsedTime * 0.12
+        mesh.rotation.x += deltaTime * 0.1
+        mesh.rotation.y += deltaTime * 0.12
     }
 
     // Render
