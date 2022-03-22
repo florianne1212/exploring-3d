163f91a7ff581bfbf99bb104c971997d8f2b35ee
Florianne Coudert <coudert.florianne.69@gmail.com>
2022-03-22 22:32:30 +0100

    Blender: cupcake cup + texture + Three.js lesson 3.2 3.3

--- Three.js/chapter_03/lesson_22/22-physics/src/script.js
+++ Three.js/chapter_03/lesson_22/22-physics/src/script.js
@@ -2,7 +2,7 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
-import CANNON from 'cannon'
+import * as CANNON from 'cannon-es'
 
 /**
  * Debug
@@ -15,11 +15,40 @@ debugObject.createSphere = () =>
         Math.random() * 0.5,
         {
             x:Math.random() -0.5 * 3,
-            y:Math.random() -0.5 * 3,
+            y:3,
             z:Math.random() -0.5 * 3
         })
 }
+debugObject.createBox = () =>
+{
+    createBox(
+        Math.random(),
+        Math.random(),
+        Math.random(),
+        {
+            x:Math.random() -0.5 * 3,
+            y:3,
+            z:Math.random() -0.5 * 3
+        })
+}
+
+debugObject.reset = () => {
+    for (const object of objectsToUpdate)
+    {
+        //remove body
+        object.body.removeEventListener('collide', playHitSound)
+        world.removeBody(object.body)
+
+        //remove mesh
+        scene.remove(object.mesh)
+       
+    }
+     objectsToUpdate.splice(0, objectsToUpdate.length)
+}
+gui.add(debugObject, 'reset')
+
 gui.add(debugObject, 'createSphere')
+gui.add(debugObject, 'createBox')
 
 /**
  * Base
@@ -29,6 +58,23 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
+
+/**
+ * Sounds
+ */
+
+const hitSound = new Audio('/sounds/hit.mp3')
+const playHitSound = (collision) =>
+{
+    const impactStrength = collision.contact.getImpactVelocityAlongNormal()
+    if(impactStrength > 1.5)
+    {
+        hitSound.volume = Math.random()
+        hitSound.currentTime = 0
+        hitSound.play()
+    }
+    
+}
 
 /**
  * Textures
@@ -51,6 +97,8 @@ const environmentMapTexture = cubeTextureLoader.load([
 
 //World
 const world = new CANNON.World()
+world.broadphase = new CANNON.SAPBroadphase(world)
+world.allowSleep = true
 world.gravity.set(0, -9.82, 0)
 
 //material
@@ -211,16 +259,49 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 const objectsToUpdate = []
 
-const createSphere = (radius, position) =>
+const BoxGeometry = new THREE.BoxBufferGeometry(1,1,1)
+const BoxMaterial = new THREE.MeshStandardMaterial({
+            metalness: 0.3,
+            roughness: 0.4,
+            envMap: environmentMapTexture
+})
+
+const createBox = (width, height, depth, position) =>
 {
-    const mesh = new THREE.Mesh(
-        new THREE.SphereBufferGeometry(radius,20,20),
-        new THREE.MeshStandardMaterial({
+    const mesh = new THREE.Mesh(BoxGeometry, BoxMaterial)
+    mesh.scale.set(width, height, depth)
+    mesh.castShadow = true
+    mesh.position.copy(position)
+    scene.add(mesh)
+
+    const shape = new CANNON.Box(new CANNON.Vec3(width/2, height/2, depth/2))
+    const body = new CANNON.Body({
+        mass: 1,
+        position: new CANNON.Vec3(0, 3, 0),
+        shape: shape,
+        material: defaultMaterial
+    })
+    body.position.copy(position)
+    body.addEventListener('collide', playHitSound)
+    world.addBody(body)
+    //save object to update
+    objectsToUpdate.push({
+        mesh: mesh,
+        body: body
+    })
+}
+
+const SphereGeometry = new THREE.SphereBufferGeometry(1,20,20)
+const sphereMaterial = new THREE.MeshStandardMaterial({
             metalness: 0.3,
             roughness: 0.4,
             envMap: environmentMapTexture
         })
-    )
+
+const createSphere = (radius, position) =>
+{
+    const mesh = new THREE.Mesh(SphereGeometry, sphereMaterial)
+    mesh.scale.set(radius, radius, radius)
     mesh.castShadow = true
     mesh.position.copy(position)
     scene.add(mesh)
@@ -233,6 +314,7 @@ const createSphere = (radius, position) =>
         material: defaultMaterial
     })
     body.position.copy(position)
+    body.addEventListener('collide', playHitSound)
     world.addBody(body)
     //save object to update
     objectsToUpdate.push({
@@ -268,6 +350,7 @@ const tick = () =>
     for (const object of objectsToUpdate)
     {
         object.mesh.position.copy(object.body.position)
+        object.mesh.quaternion.copy(object.body.quaternion)
     }
 
     //apply coordinate to our sphere
